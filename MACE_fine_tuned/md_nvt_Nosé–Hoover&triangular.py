0f01f2cbbfd7902224b1d88ad7fdb3c7d56eabd5
enkmsc <152857100+enkmsc@users.noreply.github.com>
2026-04-22 16:32:19 +0800

    Update md_nvt_Nosé–Hoover&triangular.py

--- MACE_fine_tuned/md_nvt_Nosé–Hoover&triangular.py
+++ MACE_fine_tuned/md_nvt_Nosé–Hoover&triangular.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
-from ase.io import read
+from ase.io import read, write
 from ase import units
 from ase.md.npt import NPT
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution, Stationary
+from ase.io.trajectory import Trajectory
 from mace.calculators import MACECalculator
 
 base_path = "BASEPATH"
@@ -11,6 +12,8 @@ model_path = "MODELPATH"
 structure_path = os.path.join(base_path, "POSCAR")
 trajectory_path = os.path.join(base_path, "md.traj")
 log_path = os.path.join(base_path, "logfile")
+xdatcar_path = os.path.join(base_path, "XDATCAR")
+contcar_path = os.path.join(base_path, "CONTCAR")
 
 temperature = TEMP
 starting_temperature = TEMP
@@ -19,17 +22,21 @@ taut_fs = 100.0
 pressure_gpa = 1.01325e-4
 nsteps = 500000
 
+traj_interval = 1
+log_interval = 1
+
 atoms = read(structure_path)
 
 atoms.calc = MACECalculator(
     model_paths=model_path,
-    device="cuda"
+    device="cuda",
+    enable_cueq=True,
 )
 
 MaxwellBoltzmannDistribution(
     atoms,
     temperature_K=starting_temperature,
-    force_temp=True
+    force_temp=True,
 )
 Stationary(atoms)
 
@@ -64,10 +71,16 @@ dyn = NPT(
     externalstress=pressure_gpa * units.GPa,
     ttime=taut_fs * units.fs,
     pfactor=None,
-    trajectory=trajectory_path,
     logfile=log_path,
-    loginterval=1,
-    append_trajectory=False,
+    loginterval=log_interval,
 )
 
+traj = Trajectory(trajectory_path, "w", atoms)
+dyn.attach(traj.write, interval=traj_interval)
 dyn.run(nsteps)
+
+traj.close()
+traj_read = Trajectory(trajectory_path, "r")
+write(xdatcar_path, traj_read, format="vasp-xdatcar")
+traj_read.close()
+write(contcar_path, atoms, format="vasp")
