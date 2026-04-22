70298c707ff9dfa8b5d816f708a902d2c02d8777
enkmsc <152857100+enkmsc@users.noreply.github.com>
2026-04-22 13:27:24 +0800

    Update MACE-fine-tuned-gpu-script.sh

--- MACE_fine_tuned/MACE-fine-tuned-gpu-script.sh
+++ MACE_fine_tuned/MACE-fine-tuned-gpu-script.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
-#SBATCH --job-name=fine_tuning      # Job name
+#SBATCH --job-name=MATERIAL_SEED# Job name
 #SBATCH --partition=GPU4090  # Partition name
 #SBATCH --nodes=1                  # Number of nodes
 #SBATCH --gres=gpu:1              # Request 1 GPU
 #SBATCH --ntasks=1                # Number of tasks (processes)
-#SBATCH --cpus-per-task=16       # Number of CPU cores per task
-#SBATCH --time=72:00:00           # Walltime
+#SBATCH --cpus-per-task=16      # Number of CPU cores per task
+#SBATCH --time=120:00:00           # Walltime
 
 start_time=$(date +%s)
 printf "start: $(date)\n" > record_date
-source /data01/tian_02/mace_env/bin/activate
+
+source /data01/tian_02/mace-mliap/bin/activate
 module load /data00/software/nvidia/hpc_sdk_253/modulefiles/nvhpc/25.3
+export LD_LIBRARY_PATH=/data01/tian_02/python311/lib:$CUDA_HOME/lib64:$HPCSDK/Linux_x86_64/25.3/math_libs/lib64:${LD_LIBRARY_PATH:-}
+export TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=1
 
-HOST_PATH="/data01/tian_02/MACE/fine_tune_LATP"
-TRAIN_FILE="${HOST_PATH}/train.xyz"
-TEST_FILE="${HOST_PATH}/test.xyz"
-VALID_FILE="${HOST_PATH}/valid.xyz"
-MACE_PATH="/data01/tian_02/mace_env/lib/python3.10/site-packages/mace/cli"
-MACE_TRAIN_SCRIPT="${MACE_PATH}/run_train.py"
-MACE_EVAL_SCRIPT="${MACE_PATH}/eval_configs.py"
-LAMMPS_SCRIPT="${MACE_PATH}/create_lammps_model.py"
-MACE_MODEL="/data01/tian_02/MACE/fine_tune_LATP/2023-12-10-mace-128-L0_energy_epoch-249.model"
+base_path="BASEPATH"
+cd $base_path
 
-cd "$HOST_PATH" 
+python mace_dataset.py > "$SLURM_JOB_NAME"-out
 
-python "$MACE_TRAIN_SCRIPT" \
+TRAIN=train.xyz
+VALID=valid.xyz
+TEST=test.xyz
+MACE_MODEL="MODELPATH"
+MACE_CLI=$(python -c "import mace.cli, os; print(os.path.dirname(mace.cli.__file__))")
+TRAIN_SCRIPT=$MACE_CLI/run_train.py
+EVAL_SCRIPT=$MACE_CLI/eval_configs.py
+LAMMPS_SCRIPT=$MACE_CLI/create_lammps_model.py
+
+python "$TRAIN_SCRIPT" \
     --name="MACETest" \
     --model="MACE" \
-    --train_file="$TRAIN_FILE" \
-    --valid_file="$VALID_FILE" \
-    --test_file="$TEST_FILE" \
+    --train_file="$TRAIN" \
+    --valid_file="$VALID" \
+    --test_file="$TEST" \
     --foundation_model="$MACE_MODEL" \
     --E0s="foundation" \
+    --loss="stress" \
     --energy_key="vasp_energy" \
     --forces_key="vasp_forces" \
     --stress_key="vasp_stress" \
-    --loss="stress" \
-    --energy_weight=20 \
-    --forces_weight=1000 \
-    --stress_weight=50 \
+    --energy_weight=10 \
+    --forces_weight=200 \
+    --stress_weight=30 \
+    --embedding_specs='{"vasp_magmoms": {"embed_type": "continuous", "per": "atom", "in_dim": 1, "emb_dim": 16}}' \
     --num_interactions=2 \
     --num_cutoff_basis=5 \
     --max_ell=3 \
@@ -49,28 +55,30 @@ python "$MACE_TRAIN_SCRIPT" \
     --batch_size=4 \
     --valid_batch_size=4 \
     --eval_interval=1 \
-    --max_num_epochs=500 \
+    --max_num_epochs=100 \
     --swa \
     --ema \
     --default_dtype="float64" \
     --device=cuda \
-    --seed=42 \
+    --seed=SEED \
     --save_cpu \
     --restart_latest
 
-#python $LAMMPS_SCRIPT ./MACETest_stagetwo.model --format=mliap
+python $LAMMPS_SCRIPT ./MACETest_stagetwo.model --format=mliap
 
-python "$MACE_EVAL_SCRIPT" \
-    --configs="$TEST_FILE" \
-    --model="./MACETest_stagetwo_compiled.model" \
+python "$EVAL_SCRIPT" \
+    --configs="$TEST" \
+    --model="./MACETest_stagetwo.model" \
     --default_dtype="float64" \
-    --output="./eval_test.xyz" > eval.out
+    --compute_stress \
+    --output="./eval_test.xyz" >> "$SLURM_JOB_NAME"-out
 
-python "$MACE_EVAL_SCRIPT" \
-    --configs="$TRAIN_FILE" \
-    --model="./MACETest_stagetwo_compiled.model" \
+python "$EVAL_SCRIPT" \
+    --configs="$TRAIN" \
+    --model="./MACETest_stagetwo.model" \
     --default_dtype="float64" \
-    --output="./eval_train.xyz" >> eval.out
+    --compute_stress \
+    --output="./eval_train.xyz" >> "$SLURM_JOB_NAME"-out
 
 deactivate
 
@@ -78,4 +86,3 @@ end_time=$(date +%s)
 duration=$((end_time - start_time))
 printf "end: $(date)\n" >> record_date
 printf "duration: $duration (s)\n" >> record_date
-
